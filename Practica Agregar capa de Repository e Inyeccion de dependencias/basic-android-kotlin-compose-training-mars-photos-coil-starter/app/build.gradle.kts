e22103a665e66c58ddfba13e09b3834e69185b61
edwcm <none>
2025-02-02 21:47:52 -0600

    Modificacion para el uso de Dagger e Inyecciones de dependencias

--- Practica Agregar capa de Repository e Inyeccion de dependencias/basic-android-kotlin-compose-training-mars-photos-coil-starter/app/build.gradle.kts
+++ Practica Agregar capa de Repository e Inyeccion de dependencias/basic-android-kotlin-compose-training-mars-photos-coil-starter/app/build.gradle.kts
@@ -18,6 +18,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -64,6 +66,9 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    kapt {
+        correctErrorTypes = true
+    }
 }
 
 dependencies {
@@ -84,8 +89,20 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
+    // Dagger with hilt
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("com.google.dagger:dagger:2.x")
+    kapt("com.google.dagger:dagger-compiler:2.x")
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    // Si usas ViewModel, agrega también esto:
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.x")
+    implementation("androidx.activity:activity-ktx:1.x")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
+    // testImplementation("junit:junit:4.13.2")
+    // testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
